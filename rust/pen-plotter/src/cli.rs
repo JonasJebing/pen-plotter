733ee6a553f88662ac4d44cb44039fbe80d7cfc0
Jonas Jebing <noreply.jonas.8049@protonmail.com>
2022-05-17 21:43:04 +0200

    fix parsing negative x coordinates from CLI

--- rust/pen-plotter/src/cli.rs
+++ rust/pen-plotter/src/cli.rs
@@ -6,7 +6,7 @@ const POINT_HELP: &str = "2D point in millimeter, for example 3,7 or 3.0,7.0";
 
 #[derive(Debug, Parser)]
 pub struct Cli {
-    #[clap(short, long, help = POINT_HELP, parse(try_from_str = parse_point), default_value = "0,-10")]
+    #[clap(short, long, help = POINT_HELP, parse(try_from_str = parse_point), default_value = "(0,-10)")]
     pub start: Point2<f64>,
 
     #[clap(short, long, help = POINT_HELP, parse(try_from_str = parse_point))]
@@ -35,6 +35,10 @@ pub struct Cli {
 
 fn parse_point(s: &str) -> eyre::Result<Point2<f64>> {
     let (x, y) = s
+        .strip_prefix('(')
+        .ok_or_else(|| eyre!("missing prefix '(' in {s:?}"))?
+        .strip_suffix(')')
+        .ok_or_else(|| eyre!("missing suffix ')' in {s:?}"))?
         .split_once(',')
         .ok_or_else(|| eyre!("missing delimiter ',' in {s:?}"))?;
     Ok(Point2::new(
@@ -51,20 +55,25 @@ mod tests {
 
     #[test]
     fn parse_good_point() {
-        let actual = parse_point("1,2").unwrap();
+        let actual = parse_point("(1,2)").unwrap();
         let expected = Point2::new(1.0, 2.0);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn parse_decimal_point() {
-        let actual = parse_point("4.2,9.5").unwrap();
+        let actual = parse_point("(4.2,9.5)").unwrap();
         let expected = Point2::new(4.2, 9.5);
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn parse_bad_point() {
-        let _ = parse_point("1, 2").unwrap_err();
+        let _ = parse_point("(1, 2)").unwrap_err();
+    }
+
+    #[test]
+    fn parse_negative_x_coordinate() {
+        Cli::try_parse_from(["bin-name", "--end", "(-1,0)"]).unwrap();
     }
 }
